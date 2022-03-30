7a9d9d68cabb67e6f8340d5e2012bdeb221a15d0
d.martynov <martynov@greendatasoft.ru>
2022-03-30 23:31:39 +0500

    Updates

---
+++ precache-manifest.f26be30878a18a2f6eb4ec60141e9691.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e37de646d65704d730e97733cb7f3b",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "4f7fb8f1e7ee2cc4c3b6",
+    "url": "/TodoList/static/css/main.1e0d6d27.chunk.css"
+  },
+  {
+    "revision": "5f19c4b7c0361821b7e1",
+    "url": "/TodoList/static/js/2.a7b46c21.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.a7b46c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7fb8f1e7ee2cc4c3b6",
+    "url": "/TodoList/static/js/main.c2b0013d.chunk.js"
+  },
+  {
+    "revision": "b6b11eb4357e2607567b",
+    "url": "/TodoList/static/js/runtime-main.609c3085.js"
+  }
+]);
